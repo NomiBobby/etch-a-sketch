9a33ecb0095fc61cc847552ba14e630ab26bad0b
Qiwei Mao <57202568+NomiBobby@users.noreply.github.com>
2022-12-28 17:33:19 -0500

    add preliminary version of UI

--- javascript.js
+++ javascript.js
@@ -3,8 +3,8 @@ function createGrid(gridSize){
     for (let i = 0; i < gridSize; i++) {
         const row = document.createElement("div");
         row.style.cssText = `\
-            height:${60/gridSize}%;\
-            width:60%;\
+            height:${90/gridSize}%;\
+            aspect-ratio:${gridSize / 1};
             display: flex;\
             `;
         for(let j = 0; j < gridSize; j++){
@@ -34,14 +34,12 @@ var output = document.getElementById("gridSize");
 output.innerHTML = `${slider.value}x${slider.value}`; // Display the default slider value
 
 let gridSize = slider.value;
-// let widthPct = 1/gridSize;
 createGrid(gridSize);
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
     output.innerHTML = this.value;
     let gridSize = slider.value;
-    // let widthPct = 1/gridSize;
     removeGrid();
     createGrid(gridSize);
 }
